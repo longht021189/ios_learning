e65969ec194e9ce4b986d316393b0f8c8654f327
Thanh Long <longht021189@gmail.com>
2020-08-15 10:39:21 +0700

    init project

---
+++ ios_learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_learning
+//
+//  Created by Thanh Long on 8/15/20.
+//  Copyright © 2020 Thanh Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
